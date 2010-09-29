447d678d8ed031bd0299894870651e1bf34478b3
mario <mario@153b296d-c5a6-6c43-b70c-975c6660645f>
2010-09-29 12:18:04 +0000

    added (and fixed) rspec tests fixed some bugs in process.rb after making rspec tests pass
    git-svn-id: http://ws-green-win7/svn/autoit/trunk@18 153b296d-c5a6-6c43-b70c-975c6660645f

--- lib/autoit/process.rb
+++ lib/autoit/process.rb
@@ -6,7 +6,7 @@ module AutoIt
     CACHE_TIME = 0.50
     @@procs_cached = nil
 
-    attr_reader :pid, :ppid, :path, :name, :command_line
+    attr_reader :pid, :ppid, :path, :name, :cmd_line
 
     def initialize (pid, ppid = nil, path = nil, name = nil, cmd_line = nil)
       @pid = pid
@@ -16,14 +16,62 @@ module AutoIt
       @cmd_line = cmd_line
     end
 
+    def running?
+      AutoIt::Process.running?(@pid)
+    end
+
+    def orphan?
+      running? and not AutoIt::Process.running?(@ppid)
+    end
+
     def parent
+      # system process?
+      return nil if @ppid.nil?
       AutoIt::Process.all[@ppid]
+    end
+
+    def children
+      AutoIt::Process.all.select do |pid, p|
+        p.parent == self
+      end
+    end
+
+    def ancestors 
+      ps = []
+      p = self
+      while not (p = p.parent).nil?
+        ps.push(p)
+      end
+      ps
     end
 
     def windows
       AutoIt::Window.all.select do |handle,w|
         w.process.pid == @pid
       end
+    end
+
+    def to_s
+      "Process\t[#{@pid},#{@name}]\n" \
+      "Path\t[#{@path}]\n" \
+      "CmdLine\t[#{@cmd_line}]\n" \
+      "Parent\t[#{@ppid},#{parent.nil? ? '' : parent.name}]"
+    end
+
+    def self.running? (pid)
+      AutoIt::Process.all.has_key?(pid)
+    end
+
+    def self.run (path, args = nil, options = {})
+      options = { :workingdir => nil,
+                  :flag => nil
+      }.merge(options)
+      args = [ args ].compact unless args.is_a? Array
+      cmd = "\"#{path}\""
+      cmd += " \"#{args.join('" "')}\"" unless args.empty?
+      pid = AutoIt::COM.Run(cmd, options[:workingdir], options[:flag])
+      return nil if pid == 0
+      AutoIt::Process.all(0)[pid]
     end
 
     def self.containing_window (pid = $$)
@@ -42,7 +90,10 @@ module AutoIt
         procs = WIN32OLE.connect("winmgmts:{impersonationLevel=impersonate,(Debug)}\\\\.")
         @@proc_list = {}
         procs.InstancesOf("Win32_Process").each do |p|
-          np = AutoIt::Process.new(p.ProcessId, p.ParentProcessId, p.ExecutablePath, p.Name, p.CommandLine)
+          pid = p.ProcessId
+          ppid = p.ParentProcessId
+          ppid = nil if pid == 0
+          np = AutoIt::Process.new(pid, ppid, p.ExecutablePath, p.Name, p.CommandLine)
           @@proc_list[np.pid] = np
         end
         @@procs_cached = Time.now
--- spec/autoit/process_spec.rb
+++ spec/autoit/process_spec.rb
@@ -1,20 +1,216 @@
 require 'spec_helper'
 
 describe AutoIt::Process do
-
-  before(:each) do
+  before(:all) do
+    @my_pid = ::Process.pid
+    @fixtures = File.expand_path(File.join(File.dirname(__FILE__),"..","fixtures"))
   end
 
-  context "when a new process is created" do
-    it "should be " do
-      p = AutoIt::Process.new
+  context "when listing all processes" do
+    before(:each) do
+      @procs = AutoIt::Process.all
     end
-    it "should get the name AND THEN FAILZ" do
-      # @p.get_name.should == "foo"
-      my_name = "foo"
-      my_name.should == "bar"
+
+    it "should have the pids correctly associated with respective process" do
+      @procs.each do |pid, p|
+        pid.should == p.pid
+      end
+    end
+
+    it "should have the ppids correctly associated with respective parent" do
+      @procs.each do |pid, p|
+        if p.parent.nil?
+          @procs[p.ppid].pid.should == p.ppid unless p.ppid.nil? or not @procs.has_key?(p.ppid)
+        else
+          p.ppid.should == p.parent.pid
+        end
+      end
+    end
+
+    it "should include the current process" do  
+      @procs.should include(@my_pid)
+      my_proc = @procs[@my_pid]
+      my_proc.should_not be_nil
+      my_proc.name =~ /ruby/i
+    end
+
+    it "should have all in running state" do
+      @procs.each do |pid, p|
+        p.should be_running
+        AutoIt::Process.should be_running(p.pid)
+      end
+    end
+
+    it "should have oldest ancestor as the system or an already non-existent process" do
+      @procs.each do |pid,p|
+        next if p.ancestors.empty?
+        if p.ancestors.last.pid != 0
+          p.ancestors.last.ppid.should_not be_nil
+          AutoIt::Process.should_not be_running(p.ancestors.last.ppid)
+        end
+      end
+    end
+
+    it "should not include parents of orphan processes" do
+      @procs.each do |pid,p|
+        next unless p.orphan?
+        @procs.should_not include(p.ppid)
+      end
+    end
+
+    it "should not have parents of orphan processes in running state" do
+      @procs.each do |pid,p|
+        next if p.ppid.nil? or not p.orphan?
+        AutoIt::Process.should_not be_running(p.ppid)
+      end
+    end
+
+    it "should not have a process whose parent is itself" do
+      @procs.each do |pid,p|
+        p.pid.should_not satisfy do |pid|
+          not p.parent.nil? and p.parent.pid == pid
+        end
+      end
+    end
+
+    it "should not have a process with a child as ancestor or vice-versa" do
+      @procs.each do |pid,p|
+        expand = lambda do |p, e|
+          e.should_not include(p.pid)
+          e[p.pid] = p
+          p.children.each_value { |c| expand.call(c, e) }
+        end
+        e = expand.call(p, {})
+      end
+    end
+
+    it "should not have unordered ancestors or not defined as the parent plus its ancestors" do
+      @procs.each do |pid,p|
+        ascend = lambda do |p|
+          p.parent.should == p.ancestors.first
+          if p.parent.nil?
+            p.ancestors.should be_empty
+          else
+            parent_ancestors = p.parent.ancestors
+            parent_ancestors.should == p.ancestors[1..-1]
+            parent_ancestors.each { |a| ascend.call(a) }
+          end
+        end
+        ascend.call(p)
+      end
     end
   end
 
+  context "when checking the system process" do
+    before(:each) do
+      @proc = AutoIt::Process.all[0]
+    end
+
+    it "should exist" do
+      @proc.should_not be_nil
+    end
+
+    it "should contain the default name" do
+      @proc.name.should == "System Idle Process"
+    end
+
+    it "should not have parent" do
+      @proc.ppid.should be_nil
+    end
+
+    it "should be orphan" do
+      @proc.should be_orphan
+    end
+  end
+
+  context "when running a new process" do
+    before(:each) do
+      @exec_path = File.join(@fixtures,"PlayThing.exe")
+      @cmd_line = ["arg1", "arg2"]
+      @cmd_line_str = "\"#{@cmd_line[0]}\" \"#{@cmd_line[1]}\""
+      @p = AutoIt::Process.run(@exec_path, @cmd_line)
+    end
+
+    after(:each) do
+      ::Process.kill(9, @p.pid) unless @p.nil?
+    end
+
+    it "should not be possible for non-existent executables" do
+      @p.should_not be_nil
+    end
+
+    it "should be possible for existing executables" do
+      @p.should_not be_nil
+    end
+
+    it "should be possible to get the id" do
+      @p.pid.should_not be_nil
+      @p.pid.should > 0
+    end
+
+    it "should be possible to get the parent" do
+      @p.ppid.should_not be_nil
+      @p.ppid.should == @my_pid
+      @p.parent.should_not be_nil
+      @p.parent.pid == @my_pid
+    end
+
+    it "should be possible to get the name" do
+      @p.name.should == "PlayThing.exe"
+    end
+
+    it "should be possible to get the executable's path" do
+      p1 = @p.path.gsub("\\", "/")
+      p2 = @exec_path.gsub("\\", "/")
+      p1.casecmp(p2).should == 0
+    end
+
+    it "should be possible to get the command line" do
+      p_cmd_line = @p.cmd_line
+      p_cmd_line.should_not be_nil
+      p_cmd_line.gsub!("\\", "/")
+      expected_cmd_line = @exec_path.gsub("\\", "/")
+      expected_cmd_line = "\"#{expected_cmd_line}\" #{@cmd_line_str}"
+      p_cmd_line.should == expected_cmd_line
+    end
+  end
+
+  context "when a process is running and has windows" do
+    before(:each) do
+      @exec_path = File.join(@fixtures,"PlayThing.exe")
+      @p = AutoIt::Process.run(@exec_path)
+    end
+
+    after(:each) do
+      ::Process.kill(9, @p.pid) unless @p.nil?
+    end
+
+    it "should be possible get them" do
+      sleep 0.5
+      wins = @p.windows
+      wins.should_not be_empty
+      wins2 = wins.select do |h,w|
+        w.process.pid == @p.pid and w.title == "PlayThing"
+      end
+      wins2.should_not be_empty
+    end
+  end
+
+  context "when a windowless process is running" do
+    before(:each) do
+      @exec_path = File.join(@fixtures,"KnownTitle.exe")
+      @p = AutoIt::Process.run(@exec_path)
+    end
+
+    after(:each) do
+      ::Process.kill(9, @p.pid) unless @p.nil?
+    end
+
+    it "should have no windows associated" do
+      sleep 0.5
+      wins = @p.windows
+      wins.should be_empty
+    end
+  end
 end
 
