efd5f33a44bd52b6e36a53fa021c5393fbad9bae
mario <mario@153b296d-c5a6-6c43-b70c-975c6660645f>
2010-10-01 05:30:15 +0000

    improved context reuse by creating context nesting
    git-svn-id: http://ws-green-win7/svn/autoit/trunk@44 153b296d-c5a6-6c43-b70c-975c6660645f

--- spec/autoit/process_spec.rb
+++ spec/autoit/process_spec.rb
@@ -166,80 +166,96 @@ describe AutoIt::Process do
 
   context "when running a new process" do
     before(:each) do
-      @exec_path = File.join(@fixtures,"PlayThing.exe")
+      @exec_path = File.join(@fixtures,"KnownTitle.exe")
       @cmd_line = ["arg1", "arg2"]
       @cmd_line_str = "\"#{@cmd_line[0]}\" \"#{@cmd_line[1]}\""
       @p = AutoIt::Process.run(@exec_path, @cmd_line)
     end
 
-    it "should not be possible for non-existent executables" do
-      p = AutoIt::Process.run("C:\Wow\I\Am\Not\A\Path.exe", "bad_arg!")
-      p.should be_nil
-    end
-
-    it "should be possible for existing executables" do
-      @p.should_not be_nil
-    end
-
-    it "should be possible to get the id" do
-      @p.pid.should_not be_nil
-      @p.pid.should > 0
-    end
-
-    it "should be possible to get the parent" do
-      @p.ppid.should_not be_nil
-      @p.ppid.should == @my_pid
-      @p.parent.should_not be_nil
-      @p.parent.pid == @my_pid
-    end
-
-    it "should be possible to get the name" do
-      @p.name.should == "PlayThing.exe"
-    end
-
-    it "should be possible to get the executable's path" do
-      p1 = @p.path.gsub("\\", "/")
-      p2 = @exec_path.gsub("\\", "/")
-      p1.casecmp(p2).should == 0
-    end
-
-    it "should be possible to get the command line" do
-      p_cmd_line = @p.cmd_line
-      p_cmd_line.should_not be_nil
-      p_cmd_line.gsub!("\\", "/")
-      expected_cmd_line = @exec_path.gsub("\\", "/")
-      expected_cmd_line = "\"#{expected_cmd_line}\" #{@cmd_line_str}"
-      p_cmd_line.should == expected_cmd_line
-    end
-  end
-
-  context "when a process is running and has windows" do
-    before(:each) do
-      @exec_path = File.join(@fixtures,"PlayThing.exe")
-      @p = AutoIt::Process.run(@exec_path)
-    end
-
-    it "should be possible get them" do
-      sleep 0.5
-      wins = @p.windows
-      wins.should_not be_empty
-      wins2 = wins.select do |h,w|
-        w.process.pid == @p.pid and w.title == "PlayThing"
+    context "for non-existent executables" do
+      it "should return nil" do
+        p = AutoIt::Process.run("C:\Wow\I\Am\Not\A\Path.exe", "bad_arg!")
+        p.should be_nil
       end
-      wins2.should_not be_empty
+    end
+
+    context "for an existent executable" do
+      it "should return the process" do
+        @p.should_not be_nil
+        @p.should be_instance_of AutoIt::Process
+      end
+
+      it "should be possible to get the id" do
+        @p.pid.should_not be_nil
+        @p.pid.should > 0
+      end
+
+      it "should be possible to get the parent" do
+        @p.ppid.should_not be_nil
+        @p.ppid.should == @my_pid
+        @p.parent.should_not be_nil
+        @p.parent.pid == @my_pid
+      end
+
+      it "should have the correct name" do
+        @p.name.should == "KnownTitle.exe"
+      end
+
+      it "should have the correct path to the executable" do
+        p1 = @p.path.gsub("\\", "/")
+        p2 = @exec_path.gsub("\\", "/")
+        p1.casecmp(p2).should == 0
+      end
+
+      it "should have the correct command line" do
+        p_cmd_line = @p.cmd_line
+        p_cmd_line.should_not be_nil
+        p_cmd_line.gsub!("\\", "/")
+        expected_cmd_line = @exec_path.gsub("\\", "/")
+        expected_cmd_line = "\"#{expected_cmd_line}\" #{@cmd_line_str}"
+        p_cmd_line.should == expected_cmd_line
+      end
     end
   end
 
-  context "when a windowless process is running" do
-    before(:each) do
-      @exec_path = File.join(@fixtures,"KnownTitle.exe")
-      @p = AutoIt::Process.run(@exec_path)
+  context "when checking the list of windows" do
+    context "for a process that has windows" do
+      before(:each) do
+        @exec_path = File.join(@fixtures,"PlayThing.exe")
+        @p = AutoIt::Process.run(@exec_path)
+        sleep 0.5
+        @wins = @p.windows
+      end
+
+      it "should return a non-empty list" do
+        @wins.should_not be_empty
+      end
+
+      it "should contain a window with the main title" do
+        @wins.select do |h,w|
+          w.title == "PlayThing"
+        end.should_not be_empty
+      end
+
+      it "should have all windows refering to that process" do
+        @wins.select do |h,w|
+          #puts w.process.inspect
+          w.process.pid.should == @p.pid
+        end
+      end
     end
 
-    it "should have no windows associated" do
-      sleep 0.5
-      wins = @p.windows
-      wins.should be_empty
+    context "for a process that has no windows" do
+      before(:each) do
+        @exec_path = File.join(@fixtures,"KnownTitle.exe")
+        @p = AutoIt::Process.run(@exec_path)
+      end
+
+      it "should return an empty list" do
+        sleep 0.5
+        wins = @p.windows
+        wins.should be_empty
+      end
     end
   end
 end
